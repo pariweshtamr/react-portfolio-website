8a4d17eba7c1ec7717826002f1401884d7f71dd6
Pariwesh <tamrpariwesh@gmail.com>
2022-02-24 23:44:16 +1100

    added projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
+import './projects.css'
+import { projects } from '../../constants'
 
 const Projects = () => {
-  return <section id="projects">Projects</section>
+  return (
+    <section id="projects">
+      <h5>My Recent Work</h5>
+      <h2>Projects</h2>
+
+      <div className="container projects__container">
+        {projects.map((project, index) => (
+          <article className="projects__item" key={index}>
+            <div className="projects__item-image">
+              <img src={project.image} alt="Project 1" />
+            </div>
+            <h3>{project.title}</h3>
+            <p className="projects__item-description">{project.description}</p>
+            <div className="projects__item-tech">
+              <h4>Stack</h4>
+              <div className="projects__item-tags">
+                {project.tags.map((tag, i) => (
+                  <ul className="projects__item-tag" key={i}>
+                    {tag}
+                  </ul>
+                ))}
+              </div>
+            </div>
+
+            <div className="projects__item-buttons">
+              <a
+                href={project.source}
+                className="btn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Code
+              </a>
+              <a
+                href={project.visit}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Projects
---
+++ src/constants.jsx
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    title: 'E-Commerce',
+    description:
+      'Using React, Node.js, Express & MongoDB, this project is a Full Stack MERN application that has been built from start to finish. The App is called Soccer boot store and it is an e-commerce app that allows users to purchase high-end soccer cleats from different brands all in one place.',
+    image: '/images/1.jpg',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://github.com/pariweshtamr/football-store-project.git',
+    visit: 'https://google.com',
+    id: 0,
+  },
+]
+
+export const TimeLineData = [
+  { year: 2019, text: 'Started my journey in IT' },
+  {
+    year: 2021,
+    text: 'Attained Post-graduate degree in Applied Information Technology',
+  },
+  { year: 2021, text: 'Enrolled in a MERN stack bootcamp course' },
+  { year: 2022, text: 'Sharing my projects in gitHub' },
+]
+
+export const AccompData = [
+  {
+    number: 1 + 'st',
+    text: 'First Year Course Champion at Victoria University',
+  },
+  {
+    number: 4,
+    text: 'Projects completed and shared',
+  },
+  {
+    number: 5,
+    text: 'Projects completed and shared',
+  },
+  {
+    number: 5,
+    text: 'Projects completed and shared',
+  },
+]
